ece390469fb5dbf1531d2fa722914ff1a21ededf
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-02-22 22:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// Define the function and name it functionName
+function functionName() {
+  // Use arguments.callee.name to get the name of the function
+  var name = arguments.callee.name;
+  // Display the name using an alert statement
+  alert(name);
+}
+
+// Call the function
+functionName();
